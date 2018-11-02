2706e60acf5572155627a4dcd9aa0ac03a2aef29
jaebooker <jaesonbooker@gmail.com>
2018-11-01 21:15:08 -0700

    added basic CRUD

--- controllers/posts.js
+++ controllers/posts.js
@@ -31,5 +31,27 @@ app.get('/posts/:id', (req, res) => {
     }).catch((err) => {
         console.log(err.message);
     })
-});
+})
+app.get('/posts/:id/edit', (req, res) => {
+    Post.findById(req.params.id, function(err, post) {
+        res.render('posts-edit', { post: post });
+    })
+})
+app.put('/posts/:id/', (req, res) => {
+    Post.findByIdAndUpdate(req.params.id, req.body)
+    .then(post => {
+        res.redirect(`/posts/${post._id}`)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+})
+app.delete('/posts/:id', function (req, res) {
+  console.log("It is time for this post... to end")
+  Post.findByIdAndRemove(req.params.id).then((post) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 };
